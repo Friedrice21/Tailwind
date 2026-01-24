5d36c73ac4bf7414115bd7c15b8b6b44e67e8799
Friedrice21 <bintang26nk@gmail.com>
2026-01-23 18:17:19 -0700

    tailwindcsss (2)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+      "Tions":"Tinos",
+      
+      }
+    },
+  },
+  plugins: [],
+}
+
